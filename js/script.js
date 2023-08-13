6338e4b91b5f77f9f843bab304ab3c77d0579594
jlynne01 <kate_quilloy@yahoo.com>
2023-08-13 21:18:54 +0800

    added js on checkout page

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use required'
+  
+    window.addEventListener('load', function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation')
+  
+      // Loop over them and prevent submission
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+          form.classList.add('was-validated')
+        }, false)
+      })
+    }, false)
+}())
+  
+
+function validateAndSubmit() {
+    // Get the form
+    var form = document.getElementById('needs-validation');
+
+    // Check if the form is valid
+    if (form.checkValidity()) {
+        // You can process the form here (e.g., send data to a server)
+        // For this example, we'll just show an alert
+        alert('Order has been processed!');
+    } else {
+        // Show the default validation error messages
+        form.reportValidity();
+    }
+}
